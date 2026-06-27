8d774b8b115f895c5bb047dcb3fb4c6868206b9f
Md.Raysu kobir <raysulkobir97@gmail.com>
2026-06-27 21:47:37 +0600

    add array function

--- function.js
+++ function.js
@@ -3,5 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function add(a, b) {
     return a + b;
 }
-console.log(add(10, 2));
+function printMsg(msg) {
+    console.log(msg);
+}
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(10, 20));
 //# sourceMappingURL=function.js.map
--- function.ts
+++ function.ts
@@ -1,5 +1,13 @@
-function add(a:number,b:number):number{
+function add(a:number, b:number):number{
     return a+b;
 }
 
-console.log(add(10,2));
+function printMsg(msg:string):void{
+    console.log(msg);
+}
+
+const sum=(a:number, b:number):number=>{
+    return a+b;
+}
+
+console.log(sum(10, 20));
